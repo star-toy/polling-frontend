372d6525f6bacdb543bf387918418664ed96e91d
Lin <heron6534@gmail.com>
2024-08-24 18:40:14 +0900

    feat: 라우터 설정 및 레이아웃 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import HomePage from './pages/home-page';
+import DetailPage from './components/shadcn-ui/ui/pages/detail-page';
+import Layout from './components/shadcn-ui/ui/pages/Layout';
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/detail/:id',
+        element: <DetailPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
