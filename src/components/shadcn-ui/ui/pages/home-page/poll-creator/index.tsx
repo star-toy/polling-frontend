6a79ba2bd260e4c920d516efc316180014750cf8
Lin <heron6534@gmail.com>
2024-08-24 18:46:07 +0900

    feat: PollCreator 컴포넌트 구현
    - 투표 질문 입력을 위한 Textarea 추가
    - 투표 선택지 관리를 위한 ChoiceInput 컴포넌트 추가
    - 투표 기간 설정을 위한 PollLengthSelector 컴포넌트 추가

---
+++ src/components/shadcn-ui/ui/pages/home-page/poll-creator/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Button } from '@/components/shadcn-ui/ui/button';
+import ChoiceInput from './choice-input';
+import { Textarea } from '@/components/shadcn-ui/ui/textarea';
+
+import PollLengthSelector from './poll-length-selector';
+
+const PollCreator = () => {
+  const [choices, setChoices] = useState(['', '']);
+
+  const updateChoice = (index: number, value: string) => {
+    const newChoices = [...choices];
+    newChoices[index] = value;
+    setChoices(newChoices);
+  };
+
+  const addChoice = () => {
+    setChoices([...choices, '']);
+  };
+
+  const handlePollLengthChange = (length: { days: number; hours: number; minutes: number }) => {
+    // TODO: 투표 기간 추가 로직
+    console.log(length);
+  };
+
+  return (
+    <div className="w-[512px]">
+      <div className="mb-4 h-[40px] w-full">
+        <Textarea
+          placeholder="Ask a question"
+          className="h-full resize-none border-none px-0 text-xl leading-none"
+        />
+      </div>
+      <div className="mb-4 w-full rounded-2xl border pt-3">
+        <ChoiceInput choices={choices} updateChoice={updateChoice} addChoice={addChoice} />
+        <div className="border-b" />
+        <PollLengthSelector onChange={handlePollLengthChange} />
+      </div>
+      <div className="flex justify-end">
+        <Button className="flex items-center rounded-full bg-blue-400 px-4 text-base font-semibold text-white hover:bg-blue-500">
+          Post
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PollCreator;
