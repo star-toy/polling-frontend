24c363c7c0020948e3fb9b09a181cea254e927cf
Lin <heron6534@gmail.com>
2024-09-04 00:07:05 +0900

    feat: 라우팅 설정 추가
    - 메인 페이지 컴포넌트와 레이아웃 컴포넌트 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout';
+import MainPage from './pages/main-page';
+import '../app/globals.css';
 
-createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [{ path: '/', element: <MainPage /> }],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
