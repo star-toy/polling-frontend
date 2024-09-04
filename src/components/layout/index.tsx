cd33f63f08faae963de39d45ddd2d1f0d0e3de2a
Lin <heron6534@gmail.com>
2024-09-04 13:31:52 +0900

    refactor: header에 시맨틱 태그 적용
    - 헤더 텍스트에 h1 태그 사용

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,8 +3,10 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="mx-auto w-[375px]">
-      <header className="flex h-[56px] items-center justify-center text-lg font-extrabold text-gray-800">
-        POLLHUB
+      <header>
+        <h1 className="flex h-[56px] items-center justify-center text-lg font-extrabold text-gray-800">
+          POLLHUB
+        </h1>
       </header>
       <Outlet />
     </div>
