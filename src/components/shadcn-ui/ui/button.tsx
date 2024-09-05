b5beb1739d33f8457bb70ec7a23652b5751e9c61
Lin <heron6534@gmail.com>
2024-09-05 16:18:13 +0900

    design: Button 컴포넌트의 buttonVariants에 pollCategory, pollCategory 추가

--- src/components/shadcn-ui/ui/button.tsx
+++ src/components/shadcn-ui/ui/button.tsx
@@ -9,23 +9,18 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        pollCategory: 'rounded-2xl border border-gray-300 bg-gray-50 text-body2',
+        pollOption: 'overflow-hidden rounded-lg border border-gray-200',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        pollCategory: 'px-4 py-[4.5px]',
+        pollOption: 'h-[177px] w-[166px]',
+        // TODO: icon size 추가
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: 'pollCategory',
+      size: 'pollCategory',
     },
   },
 );
