8053feb1081bd1fd0118fe9b42f287f016b091c9
Lin <heron6534@gmail.com>
2024-09-05 23:49:25 +0900

    feat: Button 컴포넌트에 variant 추가 및 font 추가
    - variant에 pollCategorySelected, ghost 추가
    - variants에 font 추가

--- src/components/shadcn-ui/ui/button.tsx
+++ src/components/shadcn-ui/ui/button.tsx
@@ -5,22 +5,34 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/components/shadcn-ui/libs';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        pollCategory: 'rounded-2xl border border-gray-300 bg-gray-50 text-body2',
-        pollOption: 'overflow-hidden rounded-lg border border-gray-200',
+        pollCategory: 'rounded-2xl border border-gray-300 bg-gray-50',
+        pollCategorySelected: 'rounded-2xl border border-black bg-black text-white',
+        ghost: 'bg-transparent',
       },
       size: {
         pollCategory: 'px-4 py-[4.5px]',
-        pollOption: 'h-[177px] w-[166px]',
+        pollOption: 'min-h-[177px] w-[166px]',
         // TODO: icon size 추가
+      },
+      font: {
+        'text-subTitle1': 'subTitle1',
+        'text-subTitle2': 'subTitle2',
+        'text-subTitle3': 'subTitle3',
+        'text-subTitle4': 'subTitle4',
+        'text-body1': 'body1',
+        'text-body2': 'body2',
+        'text-caption1': 'caption1',
+        'text-caption2': 'caption2',
       },
     },
     defaultVariants: {
       variant: 'pollCategory',
       size: 'pollCategory',
+      font: 'text-subTitle4',
     },
   },
 );
@@ -32,10 +44,14 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, font, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp
+        className={cn(buttonVariants({ variant, size, font, className }))}
+        ref={ref}
+        {...props}
+      />
     );
   },
 );
