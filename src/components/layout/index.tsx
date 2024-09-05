457c4cdcba5825db6103c903a49519d028971c5b
Lin <heron6534@gmail.com>
2024-09-05 13:34:36 +0900

    style: Layout 컴포넌트의 header 마크업 구조 개선
    - header 태그로 flex 관련 클래스 이동

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,10 +3,8 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="mx-auto w-[375px]">
-      <header>
-        <h1 className="flex h-[56px] items-center justify-center text-lg font-extrabold text-gray-800">
-          POLLHUB
-        </h1>
+      <header className="flex h-[56px] items-center justify-center">
+        <h1 className="text-lg font-extrabold text-gray-800">POLLHUB</h1>
       </header>
       <Outlet />
     </div>
