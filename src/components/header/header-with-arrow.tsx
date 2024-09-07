d1219f2e4c8188fd3f16c49156ea30fceba21f50
Jeewhan <distinct19@gmail.com>
2024-09-07 12:14:50 +0900

    feat: 지금 페이지가 첫 방문페이지일 경우, 화살표 클릭시 메인페이지행

--- src/components/header/header-with-arrow.tsx
+++ src/components/header/header-with-arrow.tsx
@@ -1,14 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import BackIcon from '../../../public/svgs/back-icon.svg?react';
+import { ROUTES } from '@/constants/routes';
 
 const NAVIGATE_BACK = -1;
 
 const HeaderWithArrow = () => {
   const navigate = useNavigate();
 
+  const handleBack = () => {
+    if (window.history.length <= 1) {
+      navigate(ROUTES.MAIN);
+    } else {
+      navigate(NAVIGATE_BACK);
+    }
+  };
+
   return (
     <header className="relative flex h-[56px] items-center justify-center border-b border-gray-100">
-      <button onClick={() => navigate(NAVIGATE_BACK)} className="absolute left-5">
+      <button onClick={handleBack} className="absolute left-5">
         <BackIcon width={24} height={24} />
       </button>
       <h1 className="text-lg font-extrabold text-gray-800">POLLHUB</h1>
