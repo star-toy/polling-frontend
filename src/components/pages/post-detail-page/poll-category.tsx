79682959653105608f240289b9a485b155264562
Lin <heron6534@gmail.com>
2024-09-07 10:15:47 +0900

    refactor: poll cateogry를 상수로 분리

--- src/components/pages/post-detail-page/poll-category.tsx
+++ src/components/pages/post-detail-page/poll-category.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Button } from '@/components/shadcn-ui/ui/button';
 
+const POLL_CATEGORIES = ['아티스트', '노래', '음악방송'];
+
 const PollCategory = () => {
   const [selectedPollCategory, setSelectedPollCategory] = useState('아티스트');
 
@@ -10,7 +12,7 @@ const PollCategory = () => {
 
   return (
     <div className="mb-6 flex gap-2">
-      {['아티스트', '노래', '음악방송'].map((category) => (
+      {POLL_CATEGORIES.map((category) => (
         <Button
           key={category}
           variant={selectedPollCategory === category ? 'pollCategorySelected' : 'pollCategory'}
