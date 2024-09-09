a8cc00a06ccf6b059a90f754505cb03b91b69a67
Jeewhan <distinct19@gmail.com>
2024-09-09 13:40:28 +0900

    style: Input font-size, placeholder font-size

--- src/components/shadcn-ui/ui/input.tsx
+++ src/components/shadcn-ui/ui/input.tsx
@@ -11,6 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           'border-input bg-background placeholder:text-muted-foreground flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          'text-base text-gray-800 placeholder:text-base',
           className,
         )}
         ref={ref}
