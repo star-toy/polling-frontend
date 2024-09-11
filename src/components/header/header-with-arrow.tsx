f9a7b1a8d0996c73641c2db1df4d1a424bc61053
Lin <heron6534@gmail.com>
2024-09-11 12:47:56 +0900

    refacotor: 뒤로가기 버튼 경로 수정
    - 메인 페이지('/')로 리다이렉트 되도록 수정

--- src/components/header/header-with-arrow.tsx
+++ src/components/header/header-with-arrow.tsx
@@ -1,14 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import BackIcon from '../../../public/svgs/back-icon.svg?react';
 
-const NAVIGATE_BACK = -1;
+const NAVIGATE_TO_MAIN = '/';
 
 const HeaderWithArrow = () => {
   const navigate = useNavigate();
 
   return (
     <header className="relative flex h-[56px] items-center justify-center border-b border-gray-100">
-      <button onClick={() => navigate(NAVIGATE_BACK)} className="absolute left-5">
+      <button onClick={() => navigate(NAVIGATE_TO_MAIN)} className="absolute left-5">
         <BackIcon width={24} height={24} />
       </button>
       <h1 className="text-lg font-extrabold text-gray-800">POLLHUB</h1>
