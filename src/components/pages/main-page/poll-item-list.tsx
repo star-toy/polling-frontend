9b697423c102034778508b8209767986dc1dd2b7
Lin <heron6534@gmail.com>
2024-09-12 12:08:25 +0900

    refactor(PollItemList): 레이아웃 조정
    - 그리드 레이아웃 크기 및 간격 조정
    - 이미지 크기 조정

--- src/components/pages/main-page/poll-item-list.tsx
+++ src/components/pages/main-page/poll-item-list.tsx
@@ -7,13 +7,13 @@ const PollItemList = () => {
     <>
       <h2 className="mb-6 ml-4 text-subTitle3">투표</h2>
       <div className="mb-14 flex justify-center">
-        <div className="grid w-[343px] grid-cols-2 gap-x-[11px] gap-y-6">
+        <div className="grid w-[358px] grid-cols-2 gap-x-[16px] gap-y-6">
           {POLL_ITEMS.map((item) => (
             <Link key={item.id} to={ROUTES.POST_DETAIL.replace(':id', item.id.toString())}>
               <img
                 src={item.image}
                 alt={item.title}
-                className="mb-2 h-[166px] w-[166px] rounded-lg object-cover"
+                className="mb-2 h-[171px] w-full rounded-lg object-cover"
               />
               <p className="line-clamp-2 text-caption1">{item.title}</p>
             </Link>
