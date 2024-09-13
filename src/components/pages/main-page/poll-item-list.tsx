5a5603ebb145aad4b6457b052c6017767b970695
Lin <heron6534@gmail.com>
2024-09-13 13:06:13 +0900

    refactor(PollItemList): POLL_ITEMS.map() 내에서 구조 분해 할당을 사용하여 코드 가독성 향상

--- src/components/pages/main-page/poll-item-list.tsx
+++ src/components/pages/main-page/poll-item-list.tsx
@@ -8,14 +8,14 @@ const PollItemList = () => {
       <h2 className="text-sub-title-3 mb-6 ml-4">투표</h2>
       <div className="mb-14 flex justify-center">
         <div className="grid w-[358px] grid-cols-2 gap-x-[16px] gap-y-6">
-          {POLL_ITEMS.map((item) => (
-            <Link key={item.id} to={createRoute.postDetail(item.id)}>
+          {POLL_ITEMS.map(({ id, image, title }) => (
+            <Link key={id} to={createRoute.postDetail(id)}>
               <img
-                src={item.image}
-                alt={item.title}
+                src={image}
+                alt={title}
                 className="mb-2 h-[171px] w-full rounded-lg object-cover"
               />
-              <p className="text-caption-1 line-clamp-2">{item.title}</p>
+              <p className="text-caption-1 line-clamp-2">{title}</p>
             </Link>
           ))}
         </div>
