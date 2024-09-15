df8bc35337a16ac7569d4fc4fb898097e6e3172f
Lin <heron6534@gmail.com>
2024-09-15 15:01:30 +0900

    refactor(MainPage): src/app/main-page 디렉토리로 이동

--- src/app/main-page/page.tsx
+++ src/app/main-page/page.tsx
@@ -1,4 +1,4 @@
-import PlusIcon from '../../public/svgs/plus-icon.svg?react';
+import Image from 'next/image';
 import Banner from '@/components/pages/main-page/banner';
 import PollItemList from '@/components/pages/main-page/poll-item-list';
 
@@ -9,7 +9,7 @@ const MainPage = () => {
       <PollItemList />
       <div className="fixed bottom-0 left-0 right-0 mx-auto w-[375px]">
         <button className="absolute bottom-[30px] right-[21px]">
-          <PlusIcon width={64} height={64} />
+          <Image src="/svgs/plus-icon.svg" alt="게시글 생성 버튼" width={64} height={64} />
         </button>
       </div>
     </>
