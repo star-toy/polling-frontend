765d1c98d43c0d998852d63259caeab88ea47a27
Lin <heron6534@gmail.com>
2024-09-19 22:23:42 +0900

    refactor(layout.tsx): 서버 컴포넌트로 변경 및 동적 헤더 분리(DynamicHeader)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
-import HeaderWithoutArrow from '@/components/header/header-without-arrow';
-import HeaderWithArrow from '@/components/header/header-with-arrow';
 import '../styles/globals.css';
+import DynamicHeader from '@/components/header/dynamic-header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathName = usePathname();
-
   return (
     <html lang="ko">
       <body>
         <div className="mx-auto w-[390px]">
-          {pathName === '/' ? <HeaderWithoutArrow /> : <HeaderWithArrow />}
+          <DynamicHeader />
           {children}
         </div>
       </body>
