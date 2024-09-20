1c766053c8953e538ed15612f9b842c093deeff1
Lin <heron6534@gmail.com>
2024-09-20 22:49:24 +0900

    remove(GetAccessTokenFromCookie): 유틸 함수 삭제

--- src/utils/get-access-token-from-cookie.ts
+++
@@ -1,8 +0,0 @@
-export const GetAccessTokenFromCookie = () => {
-  return (
-    document.cookie
-      .split('; ')
-      .find((row) => row.startsWith('accessToken='))
-      ?.split('=')[1] || null
-  );
-};
