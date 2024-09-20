35571d196036732d67d22201b53f5d6b26481d6c
Lin <heron6534@gmail.com>
2024-09-20 22:49:00 +0900

    refactor(CustomInstance): customInstance 간소화
    - GetAccessTokenFromCookie 유틸리티 및 관련 인증 로직 제거

--- src/generated/mutator/custom-instance.ts
+++ src/generated/mutator/custom-instance.ts
@@ -1,29 +1,14 @@
-import Axios, { AxiosRequestConfig, AxiosResponse, AxiosRequestHeaders } from 'axios';
+import Axios, { AxiosRequestConfig } from 'axios';
 import { API_KEY } from '@/constants/api-key';
-import { GetAccessTokenFromCookie } from '@/utils/get-access-token-from-cookie';
 
 export const AXIOS_INSTANCE = Axios.create({
   baseURL: API_KEY,
   timeout: 10000,
 });
 
-type AxiosRequester = <T>(
-  options: AxiosRequestConfig<T>,
-  includeAuth?: boolean,
-) => Promise<AxiosResponse<T>>;
+export const customInstance = async (options: AxiosRequestConfig) => {
+  const client = AXIOS_INSTANCE({ ...options });
 
-export const customInstance: AxiosRequester = async (options, includeAuth) => {
-  const headers = { ...options.headers } as AxiosRequestHeaders;
-
-  if (includeAuth) {
-    const accessToken = GetAccessTokenFromCookie();
-
-    if (accessToken) {
-      headers['Authorization'] = `Bearer ${accessToken}`;
-    }
-  }
-
-  const client = await AXIOS_INSTANCE({ ...options, headers });
-
+  await client;
   return client;
 };
