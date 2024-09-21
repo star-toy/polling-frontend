0c6b639b0bacbe4812bdbc5af4fb92ca22093adc
Lin <heron6534@gmail.com>
2024-09-21 20:01:36 +0900

    feat(api-key.ts): API 키 환경 변수(BASE_API_KEY) 추가

--- src/constants/api-key.ts
+++ src/constants/api-key.ts
@@ -1,1 +1,2 @@
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export const API_DOCS_KEY = process.env.NEXT_PUBLIC_API_DOCS_KEY;
+export const BASE_API_KEY = process.env.NEXT_PUBLIC_BASE_API_KEY;
