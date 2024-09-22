0146eb0a880330f9dc88a2d7e65404db5b8cc736
Lin <heron6534@gmail.com>
2024-09-22 09:22:35 +0900

    refactor(PollItemList): API 데이터 사용을 위한 컴포넌트 수정
    - mock data(POLL_ITEMS) 제거
    - Image 컴포넌트 주석 처리

--- src/components/pages/main-page/poll-item-list.tsx
+++ src/components/pages/main-page/poll-item-list.tsx
@@ -1,19 +1,24 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
-import { POLL_ITEMS } from '@/mock-data/post-mock-data';
+// import Image from 'next/image';
 import { createRoute } from '@/constants/routes';
+import { Post } from '@/api/generated/model';
 
-const PollItemList = () => {
+interface PollItemListProps {
+  posts: Post[] | undefined;
+}
+
+const PollItemList = ({ posts }: PollItemListProps) => {
   return (
     <>
       <h2 className="mb-6 ml-4 text-sub-title-3">투표</h2>
       <div className="mb-14 flex justify-center">
         <div className="grid w-[358px] grid-cols-2 gap-x-[16px] gap-y-6">
-          {POLL_ITEMS.map(({ id, image, title }) => (
+          {/* TODO: Image 컴포넌트 활성화 */}
+          {posts?.map(({ id, title }) => (
             <Link key={id} href={createRoute.postDetail(id)}>
-              <Image
+              {/* <Image
                 src={image}
                 alt={title}
                 width={171}
@@ -21,7 +26,7 @@ const PollItemList = () => {
                 placeholder="blur"
                 blurDataURL={image}
                 className="mb-2 rounded-lg object-cover"
-              />
+              /> */}
               <p className="line-clamp-2 text-caption-1">{title}</p>
             </Link>
           ))}
