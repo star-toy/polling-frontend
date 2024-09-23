da5ff676def867e69a3d04f9a3aceb0d8af12207
Lin <heron6534@gmail.com>
2024-09-23 17:15:44 +0900

    refactor(MainPage): PollItemList 컴포넌트를 Posts로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,12 @@ import Image from 'next/image';
 import { AxiosError } from 'axios';
 import { useFindAllPostsList } from '@/api/generated/endpoints/게시글-post/게시글-post';
 import Banner from '@/components/pages/main-page/banner';
-import PollItemList from '@/components/pages/main-page/poll-item-list';
+import Posts from '@/components/pages/main-page/posts';
 import { PostListResponse } from '@/api/generated/model';
 import '@/libs/axios-interceptor';
 
 const MainPage = () => {
-  const {
-    data: posts,
-    isLoading,
-    error,
-  } = useFindAllPostsList<PostListResponse, AxiosError | Error>();
+  const { data, isLoading, error } = useFindAllPostsList<PostListResponse, AxiosError | Error>();
 
   if (isLoading) return <div>로딩중...</div>;
   if (error) return <div>{error.message}</div>;
@@ -22,7 +18,7 @@ const MainPage = () => {
     <>
       {/* TODO: Banner 컴포넌트에 이미지 prop 전달 */}
       <Banner />
-      <PollItemList posts={posts} />
+      <Posts posts={data?.posts} />
       <div className="fixed bottom-0 left-0 right-0 mx-auto w-[375px]">
         <button className="absolute bottom-[30px] right-[21px]">
           <Image src="/svgs/plus-icon.svg" alt="게시글 생성 버튼" width={64} height={64} />
