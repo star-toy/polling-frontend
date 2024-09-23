b09fabd835238317c351aba02857d24dbc585c20
Jeewhan <distinct19@gmail.com>
2024-09-23 20:58:46 +0900

    chore: react-router-dom -> next/link

--- src/components/pages/not-found-page/index.tsx
+++ src/components/pages/not-found-page/index.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 const NotFoundPage = () => {
   return (
     <div>
       <p>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
-      <Link to="/">메인 페이지로 돌아가기</Link>
+      <Link href="/">메인 페이지로 돌아가기</Link>
     </div>
   );
 };
