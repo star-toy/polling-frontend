303636cdd9851aa6f0ca17922b820c519fd61dff
Lin <heron6534@gmail.com>
2024-09-23 15:22:49 +0900

    style(CustomInstance): Axios에서 axios로 import 구문 변경

--- src/api/generated/mutator/custom-instance.ts
+++ src/api/generated/mutator/custom-instance.ts
@@ -1,12 +1,12 @@
-import Axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_API_KEY } from '@/constants/api-key';
 
-export const AXIOS_INSTANCE = Axios.create({
+export const AXIOS_INSTANCE = axios.create({
   baseURL: BASE_API_KEY,
   timeout: 10000,
 });
 
-export const customInstance = async <T>(options: AxiosRequestConfig): Promise<T> => {
+export const CustomInstance = async <T>(options: AxiosRequestConfig): Promise<T> => {
   const client = await AXIOS_INSTANCE(options);
   return client.data;
 };
