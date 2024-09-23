7cf4e4221530004a0bb9539d05d55382e1d443e8
Lin <heron6534@gmail.com>
2024-09-23 23:05:40 +0900

    refactor(Posts): 데이터 페칭 로직 통합

--- src/components/pages/main-page/posts.tsx
+++ src/components/pages/main-page/posts.tsx
@@ -2,21 +2,25 @@
 
 import Link from 'next/link';
 // import Image from 'next/image';
+import { AxiosError } from 'axios';
+import { useFindAllPostsList } from '@/api/generated/endpoints/게시글-post/게시글-post';
 import { createRoute } from '@/constants/routes';
 import { PostDTO } from '@/api/generated/model';
+import { PostListResponse } from '@/api/generated/model';
 
-interface PostsProps {
-  posts: PostDTO[] | undefined;
-}
+const Posts = () => {
+  const { data, isLoading, error } = useFindAllPostsList<PostListResponse, AxiosError | Error>();
 
-const Posts = ({ posts }: PostsProps) => {
+  if (isLoading) return <div>로딩중...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <>
       <h2 className="mb-6 ml-4 text-sub-title-3">투표</h2>
       <div className="mb-14 flex justify-center">
         <div className="grid w-[358px] grid-cols-2 gap-x-[16px] gap-y-6">
           {/* TODO: Image 컴포넌트 활성화 */}
-          {posts?.map(({ postUid, title }) => (
+          {data?.posts?.map(({ postUid, title }: PostDTO) => (
             <Link key={postUid} href={createRoute.postDetail(postUid)}>
               {/* <Image
                 src={image}
