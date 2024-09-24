79c32949cd1300749cbd096ae1e7d5fa73fb6067
Lin <heron6534@gmail.com>
2024-09-24 14:01:03 +0900

    feat(WIP, Overlay): 득표율 오버레이 API 연동
    - Todo: 득표율 값 변경

--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -1,11 +1,11 @@
-import { PollOptionType } from '@/mock-data/post-mock-data';
+import { PollOptionDTO } from '@/api/generated/model';
 
 interface OverlayProps {
-  selectedPollOption: PollOptionType | null;
-  option: PollOptionType;
+  selectedPollOption: PollOptionDTO | null;
+  pollOption: PollOptionDTO;
 }
 
-const Overlay = ({ selectedPollOption, option }: OverlayProps) => {
+const Overlay = ({ selectedPollOption, pollOption }: OverlayProps) => {
   return (
     <div
       className={`absolute left-0 top-0 flex h-[140px] w-full items-center justify-center bg-banner-gradient transition duration-300 ease-in-out ${
@@ -14,7 +14,8 @@ const Overlay = ({ selectedPollOption, option }: OverlayProps) => {
     >
       <div className="flex flex-col items-center">
         <span className="text-caption2 text-gray-50">득표율</span>
-        <span className="text-subTitle3 text-gray-50">{option.pollingRate}%</span>
+        {/* TODO: API에서 제공하는 득표율 값으로 변경 */}
+        <span className="text-subTitle3 text-gray-50">{pollOption.pollOptionSeq}%</span>
       </div>
     </div>
   );
