ba653395c4638055a49c08bb2bf67b5bcb86fcd1
Lin <heron6534@gmail.com>
2024-10-01 20:24:55 +0900

    feat(PostDetailPage): 투표 로직 구현
    - 투표 옵션 선택 기능 추가
    - 자동 다음 카테고리 이동 기능 추가

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,10 +3,11 @@
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { AxiosError } from 'axios';
+
 import PollCategories from '@/components/pages/post-detail-page/poll-categories';
 import PollOptions from '@/components/pages/post-detail-page/poll-options';
-import { useGetPostByUid } from '@/api/generated/endpoints/게시글-post/게시글-post';
-import { PostDetailResponse } from '@/api/generated/model';
+import { useGetPostByUid } from '@/api/generated/endpoints/post/post';
+import { PostDetailResponse, PollOptionResponse } from '@/api/generated/model';
 
 interface PostDetailPageProps {
   params: {
@@ -16,19 +17,33 @@ interface PostDetailPageProps {
 
 const PostDetailPage = ({ params }: PostDetailPageProps) => {
   const id = String(params.id);
-  const {
-    data: post,
-    isLoading,
-    error,
-  } = useGetPostByUid<PostDetailResponse, AxiosError | Error>(id);
+  const { data, isLoading, error } = useGetPostByUid<PostDetailResponse, AxiosError | Error>(id);
   const [selectedPollCategory, setSelectedPollCategory] = useState<string | undefined>(undefined);
+  const [selectedPollOptions, setSelectedPollOptions] = useState<
+    Record<string, PollOptionResponse>
+  >({});
 
-  const selectedPoll = post?.polls?.find((poll) => poll.pollCategory === selectedPollCategory);
+  const selectedPoll = data?.polls?.find((poll) => poll.pollCategory === selectedPollCategory);
+
+  const handleSelectPollOption = (option: PollOptionResponse) => {
+    setSelectedPollOptions((prev) => ({ ...prev, [selectedPollCategory!]: option }));
+
+    const currentIndex = data?.polls?.findIndex(
+      (poll) => poll.pollCategory === selectedPollCategory,
+    );
+    const nextCategory = data?.polls?.[currentIndex! + 1]?.pollCategory;
+
+    if (nextCategory) {
+      setTimeout(() => {
+        setSelectedPollCategory(nextCategory);
+      }, 2000);
+    }
+  };
 
   useEffect(() => {
-    const firstCategory = post?.polls?.[0]?.pollCategory;
+    const firstCategory = data?.polls?.[0]?.pollCategory;
     setSelectedPollCategory(firstCategory);
-  }, [post]);
+  }, [data]);
 
   if (isLoading) return <div>로딩중...</div>;
   if (error) return <div>{error.message}</div>;
@@ -38,18 +53,19 @@ const PostDetailPage = ({ params }: PostDetailPageProps) => {
       {/* 투표 제목 및 설명 섹션 */}
       <div className="mx-4 mb-2 mt-6 flex flex-col gap-2">
         <h2 className="text-sub-title-2">투표</h2>
-        <p className="text-sub-title-4">{post?.title}</p>
+        <p className="text-sub-title-4">{data?.title}</p>
       </div>
 
-      {/* TODO: API에서 제공하는 이미지 값으로 변경 */}
-      <Image
-        src="/images/image(6).png"
-        alt="게시글 이미지"
-        width={390}
-        height={300}
-        placeholder="blur"
-        blurDataURL="/images/image(6).png"
-      />
+      {data?.imageUrl && (
+        <Image
+          src={data?.imageUrl}
+          alt="게시글 이미지"
+          width={390}
+          height={300}
+          placeholder="blur"
+          blurDataURL={data?.imageUrl}
+        />
+      )}
 
       {/* 투표 컨텐츠 섹션 */}
       <div className="px-4 pb-[27px] pt-8">
@@ -60,7 +76,13 @@ const PostDetailPage = ({ params }: PostDetailPageProps) => {
         />
         <p className="mb-2 text-body-1">{selectedPoll?.pollDescription}</p>
         <div className="mb-6 border border-gray-200" />
-        <PollOptions pollOptions={selectedPoll?.pollOptions} />
+
+        <PollOptions
+          selectedPoll={selectedPoll}
+          onSelectPollOption={handleSelectPollOption}
+          selectedPollOption={selectedPollOptions[selectedPollCategory!]}
+          isDisabled={!!selectedPollOptions[selectedPollCategory!]}
+        />
       </div>
     </>
   );
