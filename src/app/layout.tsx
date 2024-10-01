19f021b1e39adc467bdc8dc53353c8d69f94f22c
Lin <heron6534@gmail.com>
2024-10-01 16:41:51 +0900

    chore(clarity): Clarity 스크립트 컴포넌트로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import Script from 'next/script';
 import TanstackQueryProviders from '@/utils/tanstack-query-provider';
 import DynamicHeader from '@/components/header/dynamic-header';
+import MicrosoftClarity from '@/metrics/microsoft-clarity';
 import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -12,15 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <DynamicHeader />
             {children}
           </TanstackQueryProviders>
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
-          </Script>
+          <MicrosoftClarity />
         </div>
       </body>
     </html>
