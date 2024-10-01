62163fff361f7d36ddb2dbc0a0d02b988941e38c
Lin <heron6534@gmail.com>
2024-10-01 16:24:13 +0900

    feat(clarity): Clarity 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import TanstackQueryProviders from '@/utils/tanstack-query-provider';
 import DynamicHeader from '@/components/header/dynamic-header';
 import '../styles/globals.css';
@@ -11,6 +12,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <DynamicHeader />
             {children}
           </TanstackQueryProviders>
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+          </Script>
         </div>
       </body>
     </html>
