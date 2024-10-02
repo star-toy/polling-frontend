87415ddf697af3ca088b6f08a7dba8c243541575
Lin <heron6534@gmail.com>
2024-10-02 18:46:32 +0900

    feat(MainPageClient): 서버 사이드 렌더링 및 데이터 프리페칭 구현
    - MainPage(RootPage) 컴포넌트를 서버 컴포넌트로 변경하여 SSR 구현
    - HydrationBoundary를 통해 서버에서 프리페치한 데이터를 클라이언트로 전달

---
+++ src/components/pages/main-page/main-page-client.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import '@/libs/axios-interceptor';
+import Banner from '@/components/pages/main-page/banner';
+import Posts from '@/components/pages/main-page/posts';
+
+const MainPageClient = () => {
+  return (
+    <>
+      <Banner />
+      <Posts />
+      <div className="fixed bottom-0 left-0 right-0 mx-auto w-[375px]">
+        <Link href="/posts/new" className="absolute bottom-[30px] right-[21px]">
+          <Image src="/svgs/plus-icon.svg" alt="게시글 생성 버튼" width={64} height={64} />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default MainPageClient;
